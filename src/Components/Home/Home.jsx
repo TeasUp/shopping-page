94ec1b65f4f2add97bc8f54201ab8a0a2c2ef920
Homayoun kolagar <homayounkolagar@gmail.com>
2024-09-25 21:17:53 +0330

    fix(Navbar): update styles and refactor props handling
    - Improved the styling of the navbar for better aesthetics.
    - Corrected the repository name in the configuration.
    - Tweaked props handling in the navbar component for improved functionality.

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Navbar from "./Navbar/Navbar";
 
+const companyInfo = {
+    companyName: "Company Name",
+    companyDesc: "Company Description",
+    supportNumber: "1234567890",
+};
+
 export default function Home() {
     return (
         <>
-            <Navbar />
+            <Navbar {...companyInfo} />
         </>
     );
 }
--- src/Components/Home/Navbar/Navbar.jsx
+++ src/Components/Home/Navbar/Navbar.jsx
@@ -1,22 +1,20 @@
 import React from "react";
-import "./Navbar.css"
+import "./Navbar.css";
 
 import { SlBasket } from "react-icons/sl";
 import { BiSupport } from "react-icons/bi";
 
-export default function Navbar() {
+export default function Navbar({ companyName, companyDesc, supportNumber }) {
     return (
         <nav className="navbar">
             <ul className="nav-items">
                 <li className="nav-item nav-brand">
-                    <h4>YourNameHere</h4>
-                    <p>lil dic here!</p>
+                    <h4>{companyName}</h4>
+                    <p>{companyDesc}</p>
                 </li>
                 <li className="nav-item nav-search">
-                    <input type="text" placeholder="I want a ...." />
-                    <button>
-                        search
-                    </button>
+                    <input type="text" placeholder="I'm looking for a..." />
+                    <button>search</button>
                 </li>
                 <li className="nav-item nav-account">
                     <a href="https://www.example.com">Log In</a>
@@ -27,7 +25,7 @@ export default function Navbar() {
                 </li>
                 <li className="nav-item nav-support">
                     <BiSupport />
-                    021-12345678
+                    {supportNumber}
                 </li>
             </ul>
         </nav>
